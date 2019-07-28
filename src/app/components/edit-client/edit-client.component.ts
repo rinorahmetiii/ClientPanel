0d9a441c84b1e24df9294950b726f8765c73986b
Rinor Ahmeti <ahmeti.rinori@gmail.com>
2019-07-28 19:27:06 +0200

    Get Client by id

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client';
+import { subscribeOn } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flasjMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      this.client = client;
+    });
   }
 
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -33,7 +33,23 @@ export class ClientService {
     return this.clients;
   }
   
+  getClient(id: string): Observable<Client> {
+    this.clientDoc = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDoc.snapshotChanges().pipe(map(action => {
+      if(action.payload.exists === false) {
+        return null;
+      } else {
+        const data = action.payload.data() as Client;
+         data.id = action.payload.id;
+         return data;
+      }
+    }));
+
+    return this.client;
+  }
+
   newClient(client: Client) {
     this.clientsCollection.add(client);
   } 
+
 }
