19e330248a022b9eca63d3e01c6141b98e6c463b
Rinor Ahmeti <ahmeti.rinori@gmail.com>
2019-07-28 19:34:01 +0200

    Get Client by id

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ClientService } from '../../services/client.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages';
-import { Client } from '../../models/Client';
-import { subscribeOn } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-client',
@@ -11,25 +6,10 @@ import { subscribeOn } from 'rxjs/operators';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
-  id: string;
-  client: Client;
-  hasBalance: boolean = false;
-  showBalanceUpdateInput: boolean = false;
 
-  constructor(
-    private clientService: ClientService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private flasjMessage: FlashMessagesService
-    ) { }
+  constructor() { }
 
   ngOnInit() {
-    // Get id from url
-    this.id = this.route.snapshot.params['id'];
-    // Get client
-    this.clientService.getClient(this.id).subscribe(client => {
-      this.client = client;
-    });
   }
 
 }
